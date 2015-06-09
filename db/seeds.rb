bf366f6d6b098e742a823b4842fe714e0b81d93d
bryanelidimas <bryanelidimas@gmail.com>
2015-06-09 14:01:04 -0500

    Add 12factor gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Link.create(title: "Ex Machina Trailer", url: "https://www.youtube.com/watch?v=bggUmgeMCdc")
+Link.create(title: "Limitless Trailer", url: "https://www.youtube.com/watch?v=jOLqNOfzus4")
+Link.create(title: "The Signal Trailer ", url: "https://www.youtube.com/watch?v=gwgfeR2pMuE")
+Link.create(title: "Transcendence Official Trailer", url: "https://www.youtube.com/watch?v=VCTen3-B8GU")
